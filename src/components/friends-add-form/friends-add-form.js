e558825481f2975022b8d9b18b5698e7d4ff3783
mrkain <leks.popow@gmail.com>
2020-10-01 15:33:09 +0300

    Создан friends-add-form

---
+++ src/components/friends-add-form/friends-add-form.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+import './friends-add-form.css'
+import {render} from "react-dom";
+
+export default class FriendsAddForm extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+
+    )
+  }
+}
+
+
